abfa27bb9cecfb028aed078875c199d2b28af90d
Tomasz Smoczyński <tomeksmo@gmail.com>
2023-06-03 19:12:12 +0200

    add to cart

--- src/components/cart-icon/cart-icon-component.jsx
+++ src/components/cart-icon/cart-icon-component.jsx
@@ -4,13 +4,16 @@ import {useContext} from 'react'
 import {CartContext} from '../../context/cart.context'
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen} = useContext(CartContext)
+  const { isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext)
+  const {cartItems} = useContext(CartContext)
   const toggle = () => setIsCartOpen(!isCartOpen)
 
   return(
     <div className='cart-icon-container' onClick={toggle}>
       <ShoppingIcon className='shopping-icon'/>
-      <span className='item-count'>0</span>
+      <span className='item-count'>
+        {cartCount}
+      </span>
     </div>
   )
 
